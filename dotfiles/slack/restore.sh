e94a70acad2a708e738ce136fc9fbd268d57d0fa
Brian Chan <brian.chan@liferay.com>
2021-06-24 21:07:21 -0700

    Update Slack

--- dotfiles/slack/restore.sh
+++ dotfiles/slack/restore.sh
@@ -2,4 +2,4 @@
 
 source ../_common.sh
 
-rpm_install https://downloads.slack-edge.com/linux_releases/slack-4.13.0-0.1.fc21.x86_64.rpm
+rpm_install https://downloads.slack-edge.com/linux_releases/slack-4.17.0-0.1.fc21.x86_64.rpm
