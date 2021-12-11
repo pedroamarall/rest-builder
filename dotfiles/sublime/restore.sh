bf19466021213259cab4c2174e5d76026c09dc9f
Brian Chan <brian.chan@liferay.com>
2021-12-11 11:04:00 -0800

    Update to Sublime 4

--- dotfiles/sublime/restore.sh
+++ dotfiles/sublime/restore.sh
@@ -6,16 +6,12 @@ source ../_common.sh
 # https://forum.sublimetext.com/t/fedora-25-sublime-text-3-install/24911/2
 #
 
-sudo rm -fr ~/.config/sublime-text-3
+sudo rm -fr ~/.config/sublime-text
 sudo rm -fr /opt/sublime_text
 
-download https://download.sublimetext.com/sublime_text_3_build_3211_x64.tar.bz2
+download https://download.sublimetext.com/sublime_text_build_4121_x64.tar.xz
 
-sudo tar fx data/sublime_text_3_build_3211_x64.tar.bz2 -C /opt
-
-sudo mv /opt/sublime_text_3 /opt/sublime_text
-
-#rm data/sublime_text_3_build_3211_x64.tar.bz2
+sudo tar fx data/sublime_text_build_4121_x64.tar.xz -C /opt
 
 #
 # Add desktop shortcut.
@@ -29,9 +25,9 @@ sudo sed -i "s@Icon=sublime-text@Icon=/opt/sublime_text/Icon/256x256/sublime-tex
 # Register license.
 #
 
-mkdir -p ~/.config/sublime-text-3/Local
+mkdir -p ~/.config/sublime-text/Local
 
-cp data/License.sublime_license ~/.config/sublime-text-3/Local
+cp data/License.sublime_license ~/.config/sublime-text/Local
 
 #
 # Install Pretty JSON.
@@ -45,15 +41,15 @@ git checkout st3
 
 cd ..
 
-mkdir -p ~/.config/sublime-text-3/Packages
+mkdir -p ~/.config/sublime-text/Packages
 
-mv "Pretty JSON" ~/.config/sublime-text-3/Packages
+mv "Pretty JSON" ~/.config/sublime-text/Packages
 
 #
 # Copy settings.
 #
 
-mkdir -p ~/.config/sublime-text-3/Packages/User
+mkdir -p ~/.config/sublime-text/Packages/User
 
-cp data/*.sublime-keymap ~/.config/sublime-text-3/Packages/User
-cp data/*.sublime-settings ~/.config/sublime-text-3/Packages/User
+cp data/*.sublime-keymap ~/.config/sublime-text/Packages/User
+cp data/*.sublime-settings ~/.config/sublime-text/Packages/User
