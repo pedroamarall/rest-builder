41831ac6241dde47db0508e0a8d5e08592177ef7
Benkő Richard <richard.benko@liferay.com>
2022-07-01 13:41:36 +0200

    ISIT-30238 Adding script to check luks and user password

--- dotfiles/check_device.sh
+++ dotfiles/check_device.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-# TODO Check LUKS default/length
-# TODO Check password default/length
+function test_luks_passphrase {
+    local default_luks_password='madeofdust'
+
+    if printf "${default_luks_password}" | sudo cryptsetup luksOpen --test-passphrase /dev/sda2
+    then
+        lxterminal -t "Change disk encryption password" -e "printf \"Your disk encryption password is still set to the default.\n\" && sudo cryptsetup luksChangeKey /dev/sda2"
+    fi
+}
+
+function test_user_passphrase {
+    local default_user_password='madeofdust'
+
+    if printf "${default_user_password}" | su - me -c "echo" > /dev/null 2>&1
+    then
+        lxterminal -t "Change user password" -e "printf \"Your user password is still set to the default.\n\" && passwd"
+    fi
+}
+
+function main {
+    test_luks_passphrase
+    test_user_passphrase
+}
+
+main
