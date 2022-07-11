bfed3d57102dd289bab13ecb8626ec07634044dc
Benkő Richard <richard.benko@liferay.com>
2022-07-11 10:40:00 +0200

    ISIT-30238 Don't enforce password change; discover luks device

--- dotfiles/check_device.sh
+++ dotfiles/check_device.sh
@@ -2,10 +2,11 @@
 
 function test_luks_passphrase {
     local default_luks_password='madeofdust'
+    local default_luks_device="$(lsblk -lo PATH,FSTYPE | grep crypto_LUKS | awk '{print $1}')"
 
-    if printf "${default_luks_password}" | sudo cryptsetup luksOpen --test-passphrase /dev/sda2
+    if printf "${default_luks_password}" | sudo cryptsetup luksOpen --test-passphrase "${default_luks_device}"
     then
-        lxterminal -t "Change disk encryption password" -e "printf \"Your disk encryption password is still set to the default.\n\" && sudo cryptsetup luksChangeKey /dev/sda2"
+        lxterminal -t "Change disk encryption password" -e "printf \"Your disk encryption password is still set to the default.\n\" && sleep infinity"
     fi
 }
 
@@ -14,7 +15,7 @@ function test_user_passphrase {
 
     if printf "${default_user_password}" | su - me -c "echo" > /dev/null 2>&1
     then
-        lxterminal -t "Change user password" -e "printf \"Your user password is still set to the default.\n\" && passwd"
+        lxterminal -t "Change user password" -e "printf \"Your user password is still set to the default.\n\" && sleep infinity"
     fi
 }
 
