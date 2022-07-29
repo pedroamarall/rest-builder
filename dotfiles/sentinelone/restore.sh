ed4cfee596560349fdc6494627f844ebc5798948
Brian Chan <brian.chan@liferay.com>
2022-07-29 10:10:43 -0300

    ISIT-30239 Wire in SentinelOne (but disabled for now)

--- dotfiles/sentinelone/restore.sh
+++ dotfiles/sentinelone/restore.sh
@@ -2,6 +2,75 @@
 
 source ../_common.sh
 
-# TODO Check environment variable for region code
+function main {
+	#rpm_install SentinelAgent https://storage.googleapis.com/lfr-sentinelone-installer/SentinelAgent_linux_v22_2_1_3.rpm
 
-#rpm_install SentinelAgent https://storage.googleapis.com/lfr-sentinelone-installer/SentinelAgent_linux_v22_2_1_3.rpm
+	local serial_number=$(dmidecode -s system-serial-number)
+
+	if [[ -z ${serial_number} ]]
+	then
+		echo "Unable to determine the serial number."
+
+		exit 1
+	fi
+
+	echo sentinelctl management customer_id set "${serial_number}"
+
+	local ips=($(hostname -I))
+
+	local sentinelone_site_token
+
+	for ip in "${ips[@]}"
+	do
+		if [[ $ip == 172.168.121.* ]]
+		then
+
+			#
+			# APAC
+			#
+
+			sentinelone_site_token="eyJ1cmwiOiAiaHR0cHM6Ly9ldWNlMS0xMDUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogImZlNzIxNTRmMjUyZGJkZTUifQ=="
+
+
+			break
+		elif [[ $ip == 192.168.121.* ]]
+		then
+
+			#
+			# EU
+			#
+
+			sentinelone_site_token="eyJ1cmwiOiAiaHR0cHM6Ly9ldWNlMS0xMDUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogIjJjMGZmYTIzMTVhZDVhNTAifQ=="
+
+			break
+		elif [[ $ip == 192.168.121.* ]]
+		then
+
+			#
+			# LATAM
+			#
+
+			sentinelone_site_token="eyJ1cmwiOiAiaHR0cHM6Ly9ldWNlMS0xMDUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogImM3YzIyM2Q3NDUwMDY0YWYifQ=="
+
+			break
+		elif [[ $ip == 192.168.121.* ]]
+		then
+
+			#
+			# US
+			#
+
+			sentinelone_site_token="eyJ1cmwiOiAiaHR0cHM6Ly9ldWNlMS0xMDUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogIjBjMmI4ZDhmZDIzZDBmZGEifQ=="
+
+			break
+		else
+			echo "Unable to determine SentinelOne site token."
+
+			exit 1
+		fi
+	done
+
+	echo sentinelctl management token set ${sentinelone_site_token}
+}
+
+main
