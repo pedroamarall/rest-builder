2b8b448bb8eb5840a913ebe672adf8b9b59ed690
Brian Chan <brian.chan@liferay.com>
2022-07-29 09:52:33 -0300

    ISIT-30239 Sentinel One RPM

--- dotfiles/sentinelone/restore.sh
+++ dotfiles/sentinelone/restore.sh
@@ -3,3 +3,5 @@
 source ../_common.sh
 
 # TODO Check environment variable for region code
+
+#rpm_install SentinelAgent https://storage.googleapis.com/lfr-sentinelone-installer/SentinelAgent_linux_v22_2_1_3.rpm
