e06e0b339436caf4262a22123fe66093aa6447b6
Brian Chan <brian.chan@liferay.com>
2022-07-29 10:46:52 -0300

    ISIT-30239 Determine it dynamically based on external IP

--- dotfiles/sentinelone/restore.sh
+++ dotfiles/sentinelone/restore.sh
@@ -5,6 +5,10 @@ source ../_common.sh
 function main {
 	#rpm_install SentinelAgent https://storage.googleapis.com/lfr-sentinelone-installer/SentinelAgent_linux_v22_2_1_3.rpm
 
+	#
+	# TODO Allow users to disable SentinelOne.
+	#
+
 	local serial_number=$(dmidecode -s system-serial-number)
 
 	if [[ -z ${serial_number} ]]
@@ -16,65 +20,79 @@ function main {
 
 	echo sentinelctl management customer_id set "${serial_number}"
 
-	local ips=($(hostname -I))
+	#
+	# https://www.tecmint.com/find-linux-server-geographic-location
+	#
 
-	local sentinelone_site_token
+	local country_code=$(curl -s https://ipwho.is/$(curl -s https://ipinfo.io/ip) | jq .country_code)
 
-	for ip in "${ips[@]}"
-	do
-		if [[ $ip == 172.168.121.* ]]
-		then
+	#
+	# https://stackoverflow.com/questions/9733338/shell-script-remove-first-and-last-quote-from-a-variable
+	#
 
-			#
-			# APAC
-			#
+	country_code="${country_code%\"}"
+	country_code="${country_code#\"}"
 
-			sentinelone_site_token="eyJ1cmwiOiAiaHR0cHM6Ly9ldWNlMS0xMDUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogImZlNzIxNTRmMjUyZGJkZTUifQ=="
+	#
+	# TODO What about Morocco?
+	#
 
+	if [ ${country_code} == "AT" ] ||
+	   [ ${country_code} == "CN" ] ||
+	   [ ${country_code} == "IN" ] ||
+	   [ ${country_code} == "JP" ] ||
+	   [ ${country_code} == "SG" ]
+	then
 
-			break
-		elif [[ $ip == 192.168.121.* ]]
-		then
+		#
+		# APAC
+		#
 
-			#
-			# EU
-			#
+		sentinelone_site_token="eyJ1cmwiOiAiaHR0cHM6Ly9ldWNlMS0xMDUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogImZlNzIxNTRmMjUyZGJkZTUifQ=="
+	elif [ ${country_code} == "CZ" ] ||
+		 [ ${country_code} == "DE" ] ||
+		 [ ${country_code} == "ES" ] ||
+		 [ ${country_code} == "FI" ] ||
+		 [ ${country_code} == "FR" ] ||
+		 [ ${country_code} == "HR" ] ||
+		 [ ${country_code} == "HU" ] ||
+		 [ ${country_code} == "IT" ] ||
+		 [ ${country_code} == "PT" ]
+	then
 
-			sentinelone_site_token="eyJ1cmwiOiAiaHR0cHM6Ly9ldWNlMS0xMDUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogIjJjMGZmYTIzMTVhZDVhNTAifQ=="
+		#
+		# EU
+		#
 
-			break
-		elif [[ $ip == 192.168.121.* ]]
-		then
+		sentinelone_site_token="eyJ1cmwiOiAiaHR0cHM6Ly9ldWNlMS0xMDUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogIjJjMGZmYTIzMTVhZDVhNTAifQ=="
+	elif [ ${country_code} == "BR" ] ||
+		 [ ${country_code} == "CL" ] ||
+		 [ ${country_code} == "MX" ]
+	then
 
-			#
-			# LATAM
-			#
+		#
+		# LATAM
+		#
 
-			sentinelone_site_token="eyJ1cmwiOiAiaHR0cHM6Ly9ldWNlMS0xMDUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogImM3YzIyM2Q3NDUwMDY0YWYifQ=="
+		sentinelone_site_token="eyJ1cmwiOiAiaHR0cHM6Ly9ldWNlMS0xMDUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogImM3YzIyM2Q3NDUwMDY0YWYifQ=="
+	elif [ ${country_code} == "CA" ] ||
+		 [ ${country_code} == "US" ]
+	then
 
-			break
-		elif [[ $ip == 192.168.121.* ]]
-		then
+		#
+		# US
+		#
 
-			#
-			# US
-			#
+		sentinelone_site_token="eyJ1cmwiOiAiaHR0cHM6Ly9ldWNlMS0xMDUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogIjBjMmI4ZDhmZDIzZDBmZGEifQ=="
+	else
+		echo "Unable to determine SentinelOne site token."
 
-			sentinelone_site_token="eyJ1cmwiOiAiaHR0cHM6Ly9ldWNlMS0xMDUuc2VudGluZWxvbmUubmV0IiwgInNpdGVfa2V5IjogIjBjMmI4ZDhmZDIzZDBmZGEifQ=="
+		exit 1
+	fi
 
-			break
-		else
-			echo "Unable to determine SentinelOne site token."
-
-			exit 1
-		fi
-	done
+	echo "Register SentinelOne for ${country_code}."
 
 	echo sentinelctl management token set ${sentinelone_site_token}
-
-	#
-	# TODO Allow users to disable SentinelOne.
-	#
 }
 
 main
