300797b1f670769de439260d3a34ca8e81f023eb
Brian Chan <brian.chan@liferay.com>
2022-08-11 22:43:48 -0300

    Make adding VirtualBox repo more stable

--- dotfiles/virtualbox/restore.sh
+++ dotfiles/virtualbox/restore.sh
@@ -2,9 +2,6 @@
 
 source ../_common.sh
 
-if [ ! -e /etc/yum.repos.d/virtualbox.repo ]
-then
-	wget https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -O /etc/yum.repos.d/virtualbox.repo
-fi
+dnf_add_repo https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
 
 dnf_install VirtualBox-6.1
