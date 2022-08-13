f44c90af5315e1079a8d1c5e6a08385c793419d7
Brian Chan <brian.chan@liferay.com>
2022-08-12 21:03:15 -0300

    ISIT-30682 SF

--- dotfiles/1password/restore.sh
+++ dotfiles/1password/restore.sh
@@ -2,47 +2,38 @@
 
 source ../_common.sh
 
-#
-# https://support.1password.com/install-linux/#centos-fedora-or-red-hat-enterprise-linux
-#
+function main {
 
-echo "[1password]" > /etc/yum.repos.d/1password.repo
+	#
+	# https://support.1password.com/install-linux/#centos-fedora-or-red-hat-enterprise-linux
+	#
 
-echo "name=1Password Stable Channel" >> /etc/yum.repos.d/1password.repo
-echo "baseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch" >> /etc/yum.repos.d/1password.repo
-echo "enabled=1" >> /etc/yum.repos.d/1password.repo
-echo "gpgcheck=1" >> /etc/yum.repos.d/1password.repo
-echo "repo_gpgcheck=1" >> /etc/yum.repos.d/1password.repo
-echo "gpgkey=https://downloads.1password.com/linux/keys/1password.asc" >> /etc/yum.repos.d/1password.repo
+	echo "[1password]" > /etc/yum.repos.d/1password.repo
 
-rpm --import https://downloads.1password.com/linux/keys/1password.asc
+	echo "name=1Password Stable Channel" >> /etc/yum.repos.d/1password.repo
+	echo "baseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch" >> /etc/yum.repos.d/1password.repo
+	echo "enabled=1" >> /etc/yum.repos.d/1password.repo
+	echo "gpgcheck=1" >> /etc/yum.repos.d/1password.repo
+	echo "repo_gpgcheck=1" >> /etc/yum.repos.d/1password.repo
+	echo "gpgkey=https://downloads.1password.com/linux/keys/1password.asc" >> /etc/yum.repos.d/1password.repo
 
-dnf_install 1password 1password-cli
+	rpm --import https://downloads.1password.com/linux/keys/1password.asc
 
-#
-# Allow the 1Password SSH agent to use system authentication.
-#
-# https://www.freedesktop.org/software/polkit/docs/latest/polkit.8.html
-#
+	dnf_install 1password 1password-cli
 
-policy_file="/usr/share/polkit-1/actions/com.1password.1Password.policy"
+	#
+	# https://www.freedesktop.org/software/polkit/docs/latest/polkit.8.html
+	#
 
-restore_from_original ${policy_file}
+	local policy_file="/usr/share/polkit-1/actions/com.1password.1Password.policy"
 
-#
-# Find the line number of "com.1password.1Password.authorizeSshAgent"
-#
+	restore_from_original ${policy_file}
 
-line_num_authorize=$(grep -Fn 'com.1password.1Password.authorizeSshAgent' ${policy_file} | cut -f1 -d":")
+	local line_number_authorize=$(grep -Fn "com.1password.1Password.authorizeSshAgent" ${policy_file} | cut -d ":" -f1)
 
-#
-# Starting at the line number of "com.1password.1Password.authorizeSshAgent", find the line number of the first match of "allow_active"
-#
+	local line_number_allow_active=$(awk -v line=${line_number_authorize} 'NR > line && /allow_active/{print NR, $0}' ${policy_file} | cut -d " " -f1)
 
-line_num_allow_active=$(awk -v line=$line_num_authorize 'NR > line && /allow_active/{ print NR, $0 }' ${policy_file} | cut -f1 -d" ")
+	sed -i "${line_number_allow_active}s/auth_self/yes/" ${policy_file}
+}
 
-#
-# Use sed on that line number to replace "auth_self" with "yes"
-#
-
-sed -i "${line_num_allow_active}s/auth_self/yes/" ${policy_file}
+main
