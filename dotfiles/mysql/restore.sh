159dfa251e97a2577b1e46ea8bf4b6f9369389cf
Brian Chan <brian.chan@liferay.com>
2022-08-24 00:33:04 +0530

    Not needed

--- dotfiles/mysql/restore.sh
+++ dotfiles/mysql/restore.sh
@@ -4,4 +4,4 @@ source ../_common.sh
 
 rpm_install mysql80-community-release https://dev.mysql.com/get/mysql80-community-release-fc$(rpm -E %fedora)-1.noarch.rpm
 
-dnf_install mysql mysql-workbench
+dnf_install mysql-workbench
