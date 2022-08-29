8823e15eb118fb0304fd214f88a2399ab7df907a
Brian Chan <brian.chan@liferay.com>
2022-08-29 14:46:34 -0300

    Virtualbox

--- dotfiles/virtualbox/restore.sh
+++ dotfiles/virtualbox/restore.sh
@@ -4,4 +4,27 @@ source ../_common.sh
 
 dnf_add_repo https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
 
-dnf_install VirtualBox-6.1
+dnf remove -qy VirtualBox-*
+
+dnf install -qy "kernel-devel-uname-r == $(uname -r)"
+
+dnf_install VirtualBox akmod-VirtualBox
+
+#
+# https://www.tecmint.com/install-virtualbox-in-fedora-linux
+#
+
+#dnf_install kernel-devel qt5-qtbase qt5-qtbase-gui SDL
+
+#
+# https://forums.virtualbox.org/viewtopic.php?f=7&t=100418
+#
+
+#if [ ! -e /opt/VirtualBox ]
+#then
+#	download https://download.virtualbox.org/virtualbox/6.1.36/VirtualBox-6.1.36-152435-Linux_amd64.run
+
+#	chmod u+x data/VirtualBox-6.1.36-152435-Linux_amd64.run
+
+#	sudo ./data/VirtualBox-6.1.36-152435-Linux_amd64.run
+#fi
