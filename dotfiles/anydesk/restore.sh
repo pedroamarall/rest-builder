a63da5b70dfbd3136bbcdc42cb21cc21371f832f
Brian Chan <brian.chan@liferay.com>
2022-08-29 08:47:48 -0300

    Fix AnyDesk typo

--- dotfiles/anydesk/restore.sh
+++ dotfiles/anydesk/restore.sh
@@ -25,11 +25,11 @@ dnf_install gtkglext-libs minizip-compat
 # https://ask.fedoraproject.org/t/solved-libpangox-1-0-is-missing-in-fedora-34/16018
 #
 
-rpm_install https://kojipkgs.fedoraproject.org//packages/pangox-compat/0.0.2/15.fc31/x86_64/pangox-compat-0.0.2-15.fc31.x86_64.rpm
+rpm_install pangox-compat https://kojipkgs.fedoraproject.org//packages/pangox-compat/0.0.2/15.fc31/x86_64/pangox-compat-0.0.2-15.fc31.x86_64.rpm
 
 #
 # https://www.reddit.com/r/Fedora/comments/vnfpil/fedora_36_error_updating_anydesk
 #
 
-rpm_install https://download.anydesk.com/linux/anydesk_6.1.1-1_x86_64.rpm
+rpm_install anydesk https://download.anydesk.com/linux/anydesk_6.1.1-1_x86_64.rpm
 #dnf_install anydesk redhat-lsb-core
